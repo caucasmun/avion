bfcfeb4a1f89e50049dd5014275a448b6ca6ae71
caucasmun <vitalikslavkin250@gmail.com>
2026-06-25 18:36:12 +0300

    fixed banner's src

--- src/components/all-products-heading/AllProductsHeading.jsx
+++ src/components/all-products-heading/AllProductsHeading.jsx
@@ -1,8 +1,12 @@
 import cn from './AllProductsHeading.module.css';
 
 const AllProductsHeading = () => {
+    const bannerStyle = {
+        backgroundImage: `url('${import.meta.env.BASE_URL}all-products-heading/banner.jpg')`
+    };
+
     return (
-        <section className={cn.banner}>
+        <section className={cn.banner} style={bannerStyle}>
             <div className={cn.container}>
                 <h1 className={cn.title}>All products</h1>
             </div>
